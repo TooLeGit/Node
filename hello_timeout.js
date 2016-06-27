75d6195a1fdfa82964fa068641eccd324257f239
Levi <admin@Macs-MacBook-Pro.local>
2016-06-26 19:21:31 -0700

    hello with timeout

---
+++ hello_timeout.js
@@ -0,0 +1,10 @@
+let http = require('http');
+
+http.createSearver( function (request, response) {
+  response.writeHead(200);
+  response.write("Dog is running.");
+  setTimeout( function () {
+    response.write("Dog is done.");
+    response.end();
+  }, 5000);
+}).listen (8080);
