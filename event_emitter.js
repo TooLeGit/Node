d54efa1b47243fd68065adb5002aa04fef16ba84
Levi <admin@Macs-MacBook-Pro.local>
2016-06-26 20:35:18 -0700

    event emitter
    some event emitter stuff

---
+++ event_emitter.js
@@ -0,0 +1,10 @@
+let EventEmitter = require('events').EventEmitter;
+
+let logger = new EventEmitter();
+
+logger.on('error', (message) => console.log('ERR: ' + message));
+
+logger.emit('error', 'Spilled Milk');
+logger.emit('error', 'Eggs Cracked');
+
+logger.on('close', () => {console.log('Closing...')});
